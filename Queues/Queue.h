9dc8acf593622eb2151bc43594d9d1b6668f065b
Arius <ulibomber1@gmail.com>
2022-03-16 12:27:18 -0700

    Added header for class Queue implemented as a conatiner adapter. Renamed Queues.cpp to Lab9Main.cpp

---
+++ Queues/Queue.h
@@ -0,0 +1,23 @@
+#pragma once
+
+template <typename C>
+class Queue
+{
+public:
+	typedef typename C::value_type value_type;
+
+	Queue(){}
+
+	int size() const { return thequeue.size(); }
+
+	bool empty() const { return thequeue.empty(); }
+
+	void enqueue(const value_type& x) { thequeue.push_back(x); }
+	void dequeue() { thequeue.pop_front(); }
+
+	value_type front() { return thequeue.front(); }
+
+private:
+
+	C thequeue;
+};
